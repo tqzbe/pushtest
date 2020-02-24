b211aeb10b01ea1464726226d43a63f6b2386913
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-02-24 18:38:08 +0100

    favico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <link rel="shortcut icon" href="{{ asset('img/logo_small.png') }}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
