e1d513055801a5f71a87ab04ac0eb3853f713d6c
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-02-24 18:32:34 +0100

    Update pushController.php

--- app/Http/Controllers/pushController.php
+++ app/Http/Controllers/pushController.php
@@ -9,6 +9,6 @@ class pushController extends Controller
 {
     public function test()
     {
-        dd(OneSignal::sendNotificationToAll("Some Message", $url = 'http://tqz.be', $data =["template_id" => "b421cbe6-e8bd-4934-904e-e3c52919ec5c"]));
+        dd(OneSignal::sendNotificationToAll("Le prix de votre réparation est disponible !", $url = 'http://tqz.be', $data =["template_id" => "b421cbe6-e8bd-4934-904e-e3c52919ec5c"]));
     }
 }
